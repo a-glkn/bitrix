34d676038362208e12d7fbe6a43e1dee93e47901
Aleksandr Galkin <thebest-earnings@yandex.ru>
2022-06-27 05:15:18 +0200

    mob ver

--- js/common.js
+++ js/common.js
@@ -4,7 +4,22 @@ $(function() {
 		var pop = $(this).data('popup');
 
 		$('.overlay').fadeIn(300, function() {
-			$('.popup.popup_' + pop).fadeIn(300);
+			$('.popup.popup_' + pop).fadeIn(300, function(e) {
+
+				var h = $('.popup.popup_' + pop).outerHeight();
+                var wH = $(window).outerHeight();
+                var wS = $(window).scrollTop();
+				if(h > wH) {
+					var fromTop = wH * 0.1 + wS;
+				} else {
+					var fromTop = (wH - h) / 2 + wS;
+				}
+               
+                $('.popup.popup_' + pop).css({
+                    top: fromTop
+                });       
+
+			});
 		});
 	});
 
